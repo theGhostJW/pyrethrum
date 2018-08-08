ceb497906fd631c1f7317abdc8086983c6ae1d7e
theGhostJW <theghostjw@gmail.com>
2018-08-09 08:21:41 +1000

    hedgehog test set up

--- simple-hpack/test/Spec.hs
+++ simple-hpack/test/Spec.hs
@@ -1,8 +1,19 @@
 
 module Spec where
 
---import           Test.Tasty.Hedgehog
+import           Foundation
+import           Hedgehog
+import qualified Hedgehog.Gen        as Gen
+import qualified Hedgehog.Range      as Range
+import           Test.Tasty.Hedgehog ()
 import           Test.Tasty.HUnit
 -- import           Test.Tasty.TH
 
 unit_try_me = 1 @=? 1
+
+
+hprop_try_me_hedgehog :: Property
+hprop_try_me_hedgehog =
+  property $ do
+    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+    reverse (reverse xs) === xs

237663b87b2f16b486fc0ef54b35fab03b06be51
theGhostJW <theghostjw@gmail.com>
2018-08-11 17:43:40 +1000

    simple example not working yet - type signatures change

--- src/InteractorSpike.hs
+++ src/InteractorSpike.hs
@@ -27,8 +27,8 @@ data Console r where
   GetLine     :: Console String
   ExitSuccess :: Console ()
 
-putStrLn :: Member Console effs => String -> Eff effs ()
-putStrLn = send . PutStrLn
+printString :: Member Console effs => String -> Eff effs ()
+printString = send . PutStrLn
 
 getLine :: Member Console effs => Eff effs String
 getLine = send GetLine
@@ -40,10 +40,40 @@ exitSuccess = send ExitSuccess
 --------------------------------------------------------------------------------
                           -- Effectful Interpreter --
 --------------------------------------------------------------------------------
-runConsole :: Eff [Console, IO] a -> IO a
+
+runConsole :: Eff '[Console, IO] a -> IO a
 runConsole = runM . interpretM (
-  \case
-    PutStrLn msg -> IOOps.putStrLn msg
-    GetLine -> toString <$> Prelude.getLine
-    ExitSuccess ->  SysExit.exitSuccess
-    )
+                                  \case
+                                    PutStrLn msg -> IOOps.putStrLn msg
+                                    GetLine -> toString <$> Prelude.getLine
+                                    ExitSuccess ->  SysExit.exitSuccess
+                               )
+
+--------------------------------------------------------------------------------
+                            -- Pure Interpreter --
+--------------------------------------------------------------------------------
+
+runConsolePure :: [String] -> Eff '[Console] w -> [String]
+runConsolePure inputs instructions =  let
+                                       invoke :: Console v -> Eff '[Error (), State [String], Writer [String]] v
+                                       invoke (PutStrLn msg) = tell [msg]
+                                       invoke GetLine = do
+                                                     nextLine <- get
+                                                     case nextLine of
+                                                       [] -> error "not enough lines"
+                                                       (x:xs) -> put xs >> pure x
+                                       invoke ExitSuccess = throwError ()
+
+                                       result = run (runWriter (runState inputs (runError (reinterpret3 invoke instructions))))
+                                     in
+                                       snd . fst $ result
+
+demoInstructions :: Eff '[Console, IO] ()
+demoInstructions = do
+                      printString "What is your name? : "
+                      name <- getLine
+                      printString $ "Hello " <> name <> " have a nice day !!"
+
+
+-- demoPure = runConsolePure ["John"] demoInstructions
+demoEffectful = runConsole demoInstructions

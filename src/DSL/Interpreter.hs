2e2d7f290b5ddeb595ff2b55efde1552dfc4cf19
theGhostJW <theghostjw@gmail.com>
2018-09-10 10:35:57 +1000

    more refactoring

--- src/DSL/Interpreter.hs
+++ src/DSL/Interpreter.hs
@@ -2,9 +2,26 @@
 module DSL.Interpreter where
 
 import           Control.Monad.Freer
-import           DSL.Ensure
+import           Control.Monad.Freer.Error
+import           Control.Monad.Freer.Writer
 import           DSL.FileSystem
+import           DSL.Ensure
 import           Foundation.Extended
 import TestItem
+import AppError
 
-type InteractorFileSystem runConfig item apState = forall effs. (Members '[Ensure, FileSystem] effs, TestItem item) => runConfig -> item -> Eff effs apState
+type InteractorFileSystem runConfig item r = forall effs. (Members '[Ensure, FileSystem] effs, TestItem item) => runConfig -> item -> Eff effs r
+
+executeFileSystemInIO :: (a -> v) -> Eff '[FileSystem, Ensure, Error AppError, IO] a -> IO (Either AppError v)
+executeFileSystemInIO func app = runM $ runError
+                            $ ensureInterpreter
+                            $ fileSystemIOInterpreter
+                            $ func <$> app
+
+executeFileSystemDocument :: (a -> b) -> Eff '[FileSystem, Ensure, Error AppError, Writer [String]] a -> (Either AppError b, [String])
+executeFileSystemDocument func app = run
+                              $ runWriter
+                              $ runError
+                              $ ensureInterpreter
+                              $ interpret fileSystemDocInterpreter
+                              $ func <$> app
--- src/RoughTest.hs
+++ src/RoughTest.hs
@@ -4,53 +4,54 @@
 
 module RoughTest where
 
-import DSL.Interpreter
-import TestItem
-import qualified Control.Monad as Monad
+import           AppError
+import           Control.Exception
+import qualified Control.Monad                   as Monad
 import           Control.Monad.Freer
 import           Control.Monad.Freer.Coroutine
 import           Control.Monad.Freer.Error
+import           Control.Monad.Freer.Reader
 import           Control.Monad.Freer.State
 import           Control.Monad.Freer.Writer
-import           Control.Monad.Freer.Reader
-import           Data.Function                 ((&))
+import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Either.Exit (orDie)
+import           Data.Function                   ((&))
 import           Data.Functor
 import           Data.List
-import           Foundation.Extended           hiding (putStrLn, readFile, writeFile, fail, Item)
-import    qualified       Foundation.Extended  as F
+import           Data.Map
+import           DSL.Ensure
+import           DSL.FileSystem
+import           DSL.Interpreter
+import           Foundation.Extended             hiding (Item, fail, putStrLn,
+                                                  readFile, writeFile)
+import qualified Foundation.Extended             as F
 import           Foundation.String
 import           Paths_pyrethrum
-import           Control.Monad.Trans.Either
-import DSL.Ensure
-import DSL.FileSystem
-import AppError
-import Runner
-import Data.Map
-import           Control.Monad.Trans.Either.Exit (orDie)
 import qualified Prelude
-import           System.Exit                   as SysExit hiding (ExitCode (ExitSuccess))
+import           Runner
+import           System.Exit                     as SysExit hiding (ExitCode (ExitSuccess))
 import           System.IO                       (FilePath, IO,
                                                   IOMode (ReadMode, WriteMode),
                                                   withFile)
 import           System.IO.Error                 (isAlreadyInUseError,
                                                   isDoesNotExistError,
                                                   isPermissionError)
-import Control.Exception
+import           TestItem
 
 default (String)
 
 {- Application (Interactor) -}
 
 data ApState = ApState {
-  itemId :: Int,
+  itemId   :: Int,
   filePath :: Path Abs File,
   fileText :: StrictReadResult
 }
   deriving Show
 
 data Item = Item {
-                    iid :: Int,
-                    pre :: String,
+                    iid  :: Int,
+                    pre  :: String,
                     post :: String,
                     path :: Path Abs File
                   } deriving Show
@@ -63,8 +64,8 @@ newtype ValState = ValState {
 
 data RunConfig = RunConfig {
   environment :: String,
-  depth :: Integer,
-  path :: Path Abs File
+  depth       :: Integer,
+  path        :: Path Abs File
 }
 
 interactor :: InteractorFileSystem RunConfig Item ApState
@@ -74,7 +75,6 @@ interactor runConfig item = do
                               ensure True "Blahh"
                               txt <- readFile fullFilePath
                               pure $ ApState (iid item) fullFilePath txt
-
 
 prepState :: RunConfig -> ApState -> ValState
 prepState r a = ValState $ 10 * itemId a
@@ -94,69 +94,54 @@ sampleRunConfig = RunConfig {
   path = [absfile|C:\Vids\SystemDesign\VidList.txt|]
 }
 
-
-executeInIO :: (a -> v) -> Eff '[FileSystem, Ensure, Error AppError, IO] a -> IO (Either AppError v)
-executeInIO func app = runM $ runError
-                            $ ensureInterpreter
-                            $ fileSystemIOInterpreter
-                            $ func <$> app
-
 -- Demos
 replShow d = Prelude.sequenceA $ Prelude.sequenceA <$> d
 
-demoExecuteInIO :: IO (Either AppError ValState)
-demoExecuteInIO = executeInIO (prepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
+demoExecuteFileSystemInIO :: IO (Either AppError ValState)
+demoExecuteFileSystemInIO = executeFileSystemInIO (prepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
 
 returnValState apState valState = valState
 
 demoIOAll :: Either FilterError [IO (Either AppError ValState)]
-demoIOAll = runTest returnValState sampleRunConfig prepState interactor sampleTestItems executeInIO All
+demoIOAll = runTest returnValState sampleRunConfig prepState interactor sampleTestItems executeFileSystemInIO All
 
 demoIOAllRepl :: IO (Either FilterError [Either AppError ValState])
 demoIOAllRepl = replShow demoIOAll
 
 
 demoIOFull :: Either FilterError [IO (Either AppError (TestInfo ApState ValState))]
-demoIOFull = runTest TestInfo sampleRunConfig prepState interactor sampleTestItems executeInIO All
+demoIOFull = runTest TestInfo sampleRunConfig prepState interactor sampleTestItems executeFileSystemInIO All
 
 demoIOFullRepl :: IO (Either FilterError [Either AppError (TestInfo ApState ValState)])
 demoIOFullRepl = replShow demoIOFull
 
 dummyPrepState r a = a
 
-demoExecuteInIONoVal :: IO (Either AppError ApState)
-demoExecuteInIONoVal = executeInIO (dummyPrepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
+demoExecuteFileSystemInIONoVal :: IO (Either AppError ApState)
+demoExecuteFileSystemInIONoVal = executeFileSystemInIO (dummyPrepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
 
 returnApState apState valState = apState
 
 demoIOAllNoVal:: Either FilterError [IO (Either AppError ApState)]
-demoIOAllNoVal = runTest returnApState sampleRunConfig prepState interactor sampleTestItems executeInIO All
+demoIOAllNoVal = runTest returnApState sampleRunConfig prepState interactor sampleTestItems executeFileSystemInIO All
 
 demoIOAllNoValRepl ::  IO (Either FilterError [Either AppError ApState])
 demoIOAllNoValRepl = replShow demoIOAllNoVal
 
--- demoIOAllValidate = runTest prepState sampleRunConfig interactor sampleTestItems executeInIO All
-
-executeDocumented :: (a -> b) -> Eff '[FileSystem, Ensure, Error AppError, Writer [String]] a -> (Either AppError b, [String])
-executeDocumented func app = run
-                              $ runWriter
-                              $ runError
-                              $ ensureInterpreter
-                              $ interpret fileSystemDocInterpreter
-                              $ func <$> app
+-- demoIOAllValidate = runTest prepState sampleRunConfig interactor sampleTestItems executeFileSystemInIO All
 
 -- Demos
 demoDocument :: (Either AppError ValState, [String])
-demoDocument = executeDocumented (prepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
+demoDocument = executeFileSystemDocument (prepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
 
 demoDocumentedAll :: Either FilterError [(Either AppError ValState, [String])]
-demoDocumentedAll = runTest returnValState sampleRunConfig prepState interactor sampleTestItems executeDocumented  All
+demoDocumentedAll = runTest returnValState sampleRunConfig prepState interactor sampleTestItems executeFileSystemDocument  All
 
 demoDocumentNoVal :: (Either AppError ApState, [String])
-demoDocumentNoVal = executeDocumented (dummyPrepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
+demoDocumentNoVal = executeFileSystemDocument (dummyPrepState sampleRunConfig) (interactor sampleRunConfig sampleItem)
 
 demoDocumentedAllNoVal :: Either FilterError [(Either AppError ApState, [String])]
-demoDocumentedAllNoVal = runTest returnApState sampleRunConfig prepState interactor sampleTestItems executeDocumented All
+demoDocumentedAllNoVal = runTest returnApState sampleRunConfig prepState interactor sampleTestItems executeFileSystemDocument All
 
 instance TestItem Item where
   identifier = iid

28806d9fc942852efc7b34faf9167b77280a2911
theGhostJW <theghostjw@gmail.com>
2018-09-11 15:31:08 +1000

    started Check

--- src/Check.hs
+++ src/Check.hs
@@ -3,4 +3,8 @@ module Check where
 
 import           Foundation.Extended
 
-data CheckResult = Success
+data CheckItemResult = Success String |
+                       Failure String |
+                       Skipped String |
+                       Exception String
+                       deriving (Show, Eq)
---
+++ test/CheckTest.hs
@@ -0,0 +1,9 @@
+
+module CheckTest where
+
+import           Check
+import           Data.List.Safe  as SafeList
+import           Foundation      hiding (Item)
+import qualified Prelude
+import           Runner.Internal
+import           Test.Extended

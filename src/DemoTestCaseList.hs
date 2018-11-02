ced2e33c62e8f0add1250c57be61d4d8e9753e22
theGhostJW <theghostjw@gmail.com>
2018-11-02 12:24:09 +1100

    removed filterTestsLocal

--- src/DemoTestCaseList.hs
+++ src/DemoTestCaseList.hs
@@ -19,13 +19,6 @@ import           Runner as R
 runInIO = testRun [] runConfig testInfoFull executeInIO
 runDocument  = testRun [] runConfig testInfoFull executeDocument
 
-filterTestsLocal :: forall effs. EFFFileSystem effs =>
-      (forall i as vs. TestFilters RunConfig TestConfig -> RunConfig -> GenericTest TestConfig RunConfig i effs as vs -> Identity (TestFilterResult TestConfig))
-      -> TestFilters RunConfig TestConfig
-      -> RunConfig
-      -> [TestFilterResult TestConfig]
-filterTestsLocal = filterTests runRunner
-
 testRun :: forall effs m. (EFFFileSystem effs, Monad m) =>
                   TestFilters RunConfig TestConfig                                          -- test filters
                   -> RunConfig                                                              -- runConfig
@@ -35,7 +28,7 @@ testRun :: forall effs m. (EFFFileSystem effs, Monad m) =>
 testRun fltrs r agg itpr =
                       let
                         filterTests' :: (forall i as vs. TestFilters RunConfig TestConfig -> RunConfig -> GenericTest TestConfig RunConfig i effs as vs -> Identity (TestFilterResult TestConfig)) -> [TestFilterResult TestConfig]
-                        filterTests' ff = filterTestsLocal ff fltrs r
+                        filterTests' ff = filterTests runRunner ff fltrs r
 
                         fltrLog :: [TestFilterResult TestConfig]
                         fltrLog = filterTests' filterTest

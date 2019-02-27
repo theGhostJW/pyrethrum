9b65df111c5c1afe16eb7bdb76c49a4c79dd2520
theGhostJW <theghostjw@gmail.com>
2019-02-27 16:46:22 +1100

    skeleton for processing log file lines - no tests

--- src/LogTransformation.hs
+++ src/LogTransformation.hs
@@ -2,6 +2,7 @@ module LogTransformation where
 
 import Common
 import Foundation.Extended
+import Foundation.Compat.ByteString
 import System.IO
 import qualified Prelude as P
 import Basement.String
@@ -9,51 +10,79 @@ import AuxFiles
 import DSL.LogProtocol
 import Text.Show.Pretty as PP
 import qualified Data.Aeson as A
+import Data.Aeson.TH
+import Data.ByteString.Char8 as B
 
 -- TODO: re-implement with streams
-runLines :: Step accum itm rslt -> IParser itm -> Printer rslt -> accum -> AbsFile -> IO ()
-runLines step ipsr prnter seed file = safeOpenFile file ReadMode
+runLines :: forall accum err itm rslt. Step accum itm err rslt 
+                                      -> IParser err itm 
+                                      -> ISerialiser rslt 
+                                      -> ISerialiser err 
+                                      -> accum 
+                                      -> AbsFile 
+                                      -> IO ()
+runLines step ipsr rsltSersr errSersr seed file = safeOpenFile file ReadMode
                     >>= either 
                           P.print
-                          (\hIn -> mainloop step ipsr prnter hIn stdout 1 seed)
+                          (\hIn -> mainloop step ipsr rsltSersr errSersr hIn stdout 1 seed)
 
-mainloop :: forall accum itm rslt. Step accum itm rslt -> IParser itm -> Printer rslt -> Handle -> Handle -> Int -> accum -> IO ()
-mainloop step ipsr prnter inh outh lineNo accum =
+mainloop :: forall accum err itm rslt. Step accum itm err rslt 
+                                    -> IParser err itm 
+                                    -> ISerialiser rslt 
+                                    -> ISerialiser err 
+                                    -> Handle 
+                                    -> Handle 
+                                    -> Int 
+                                    -> accum 
+                                    -> IO ()
+mainloop step ipsr rsltSersr errSersr inh outh lineNo accum =
     let 
       localLoop :: Int -> accum -> IO () 
-      localLoop = mainloop step ipsr prnter inh outh 
+      localLoop = mainloop step ipsr rsltSersr errSersr inh outh 
+
+      output :: B.ByteString -> IO ()
+      output = B.hPutStrLn outh
     in
       hIsEOF inh >>=
                   bool
                       (pure ())
                       (
                       do 
-                        inpStr <- hGetLine inh
+                        byteLine <- B.hGetLine inh
                         let 
-                          (nxtAccum, result) = step lineNo accum $ ipsr inpStr
+                          (nxtAccum, result) = step lineNo accum (ipsr byteLine)
                         
-                        maybef result
-                          (pure ())
-                          (hPutStrLn outh . prnter)
+                        eitherf result
+                          (output . errSersr)
+                          (maybe
+                            (pure ())
+                            (output . rsltSersr)
+                          )
                           
                         localLoop (lineNo + 1) nxtAccum
                       )
             
+type Step accum itm err rslt = Int -> accum -> Either err itm -> (accum, Either err (Maybe rslt))
+type ISerialiser src = src -> ByteString
+type IParser err itm = ByteString -> Either err itm
 
-type Step accum itm rslt = Int -> accum -> itm -> (accum, Maybe rslt)
-type Printer rslt = rslt -> P.String
-type IParser itm = P.String -> itm
+data IterationAccumulator = IterationAccumulator
 
 data TestIteraion = Iteration |
                     OutOfIterationError AppError |
                     ParseError AppError
                     deriving (Show, Eq)
 
-iterationStep :: Int -> String -> LogProtocol -> (String, Maybe TestIteraion)
-iterationStep linNo accum lp = ("Test", Just Iteration)
+$(deriveJSON defaultOptions ''TestIteraion)
 
-itrPrint :: TestIteraion -> P.String
-itrPrint = ppShow
+iterationStep :: Step IterationAccumulator LogProtocol String TestIteraion
+iterationStep linNo accum lp = (accum, Right $ Just Iteration)
 
--- lpParser :: P.String -> LogProtocol
--- lpParser = _ --A.decode . toList
+itrSerialise :: ISerialiser TestIteraion
+itrSerialise = toS . A.encode
+
+strSerialise :: ISerialiser String
+strSerialise = toByteString . toBytes UTF8 
+
+lpParser :: ByteString -> Either String LogProtocol
+lpParser bs = mapLeft toS $ A.eitherDecode $ toS bs

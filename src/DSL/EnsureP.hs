602ff39cc5d5a6a98ab5761e3969b9e7e2eaa06e
theGhostJW <theghostjw@gmail.com>
2019-07-31 10:43:59 +1000

    ensure switched to polysemy

--- src/DSL/EnsureP.hs
+++ src/DSL/EnsureP.hs
@@ -4,11 +4,9 @@ module DSL.EnsureP where
 import Common
 import DSL.Logger
 import           Pyrelude
--- import           Control.Monad.Freer
--- import           Control.Monad.Freer.Error
 import qualified Control.Monad as Monad
 import Polysemy
-import Polysemy.Error
+import Polysemy.Error as PE
 
 data Ensure m a where
   Ensure :: Text -> Bool -> Ensure m ()
@@ -16,20 +14,20 @@ data Ensure m a where
 
 makeSem ''Ensure
 
--- type Ensurable m a = Sem '[Error EnsureError] a
+type Ensurable a = Sem '[Ensure, Error EnsureError] a
 
--- ensureInterpreter :: forall effs a. Member (Error EnsureError) effs => Sem (Ensure ': effs) a -> Sem effs a
--- ensureInterpreter = interpret $ \case
---                                     Ensure message condition -> Monad.unless condition $ throwError $ EnsureError message
---                                     DSL.EnsureP.Throw message -> throwError $ EnsureError message
+ensureInterpreter :: forall effs a. Member (Error EnsureError) effs => Sem (Ensure ': effs) a -> Sem effs a
+ensureInterpreter = interpret $ \case
+                                    Ensure message condition -> Monad.unless condition $ PE.throw $ EnsureError message
+                                    DSL.EnsureP.Throw message -> PE.throw $ EnsureError message
 
--- -- when documenting actions we do nothing as ensure and throw 
--- -- are just programming constructs, they don't represent user actions
--- ensureDocInterpreter :: forall effs a. Sem (Ensure ': effs) a -> Sem effs a
--- ensureDocInterpreter = interpret $ \case
---                                     Ensure message condition -> pure ()
---                                     DSL.EnsureP.Throw message -> pure ()
+-- when documenting actions we do nothing as ensure and throw 
+-- are just programming constructs, they don't represent user actions
+ensureDocInterpreter :: forall effs a. Sem (Ensure ': effs) a -> Sem effs a
+ensureDocInterpreter = interpret $ \case
+                                      Ensure message condition -> pure ()
+                                      DSL.EnsureP.Throw message -> pure ()
 
--- fullEnsureInterpreter :: Sem '[Ensure, Error EnsureError] a -> Either EnsureError a
--- fullEnsureInterpreter effs = run $ runError $ ensureInterpreter effs
+fullEnsureInterpreter :: Sem '[Ensure, Error EnsureError] a -> Either EnsureError a
+fullEnsureInterpreter effs = run . runError $ ensureInterpreter effs
 

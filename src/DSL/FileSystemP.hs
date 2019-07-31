94bd8301ab7743a9c5460fb1a096c2dd2ced7648
theGhostJW <theghostjw@gmail.com>
2019-07-31 10:55:49 +1000

    fileSystem checged to polysemy

--- src/DSL/FileSystemP.hs
+++ src/DSL/FileSystemP.hs
@@ -4,11 +4,8 @@ import Common
 import           Pyrelude as P
 import           Pyrelude.IO as PO
 import           Data.Functor
--- import           Control.Monad.Freer
--- import           Control.Monad.Freer.Error
--- import           Control.Monad.Freer.Writer
 import           Control.Exception as E
-import           DSL.Logger
+import           DSL.LoggerP
 import           DSL.LogProtocol
 import Polysemy
 import Polysemy.Input
@@ -25,7 +22,7 @@ makeSem ''FileSystem
 
 -- {- File System IO Interpreter -}
 
-fileSystemIOInterpreter :: forall effs a.  Members '[Error FileSystemError, Embed IO] effs => Sem (FileSystem ': effs) a -> Sem effs a
+fileSystemIOInterpreter :: forall effs a. Members '[Error FileSystemError, Embed IO] effs => Sem (FileSystem ': effs) a -> Sem effs a
 fileSystemIOInterpreter =
                           let
                             handleException :: forall b. IO b -> (IOException -> FileSystemError) -> Sem effs b
@@ -39,16 +36,16 @@ fileSystemIOInterpreter =
                                           ReadFile path -> handleException (PO.readFile $ toFilePath path) ReadFileError
                                           WriteFile path str -> handleException (PO.writeFile (toFilePath path) str) WriteFileError
 
--- fileSystemDocInterpreter :: Member Logger effs => Eff (FileSystem ': effs) a -> Eff effs a
--- fileSystemDocInterpreter = interpret $
---                                       let
---                                         mockContents :: Text
---                                         mockContents = "Mock File Contents"
---                                       in
---                                         \case
---                                           ReadFile path ->
---                                             logItem (IterationLog . Doc . DocAction $ ActionInfo ("readFile: " <> txt path) ) $> mockContents
+fileSystemDocInterpreter :: Member Logger effs => Sem (FileSystem ': effs) a -> Sem effs a
+fileSystemDocInterpreter = interpret $
+                                      let
+                                        mockContents :: Text
+                                        mockContents = "Mock File Contents"
+                                      in
+                                        \case
+                                          ReadFile path ->
+                                            logItem (IterationLog . Doc . DocAction $ ActionInfo ("readFile: " <> txt path) ) $> mockContents
 
---                                           WriteFile path str -> logItem . IterationLog . Doc . DocAction $ ActionInfoM 
---                                                                                         ("write file: " <> txt path)
---                                                                                         $ "contents:\n" <> str
+                                          WriteFile path str -> logItem . IterationLog . Doc . DocAction $ ActionInfoM 
+                                                                                        ("write file: " <> txt path)
+                                                                                        $ "contents:\n" <> str

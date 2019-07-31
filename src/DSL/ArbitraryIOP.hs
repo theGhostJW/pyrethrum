21d8c6a35079be1548e55fa816b9e19129697fd7
theGhostJW <theghostjw@gmail.com>
2019-07-31 11:14:13 +1000

    arbitartyIOP done

--- src/DSL/ArbitraryIOP.hs
+++ src/DSL/ArbitraryIOP.hs
@@ -2,7 +2,7 @@ module DSL.ArbitraryIOP where
 
 import Common
 import DSL.LogProtocol
-import DSL.Logger
+import DSL.LoggerP
 import           Pyrelude
 -- import           Control.Monad.Freer
 -- import           Control.Monad.Freer.Error
@@ -12,7 +12,7 @@ import           Data.Functor
 import Polysemy
 import Polysemy.Input
 import Polysemy.Output
-import Polysemy.Error
+import Polysemy.Error as PE
 
 
 data ArbitraryIO m a where
@@ -20,20 +20,18 @@ data ArbitraryIO m a where
 
 makeSem ''ArbitraryIO
 
+arbitraryIODocInterpreter :: forall effs a. Member Logger effs => Sem (ArbitraryIO ': effs) a -> Sem effs a
+arbitraryIODocInterpreter = interpret $ \(ArbitraryIO msg def _) -> logItem (IterationLog . Doc $ DocIOAction msg) $> def
 
--- arbitraryIODocInterpreter :: forall effs a. Member Logger effs => Sem (ArbitraryIO ': effs) a -> Sem effs a
--- arbitraryIODocInterpreter = interpret $ \(ArbitraryIO msg def _) -> logItem (IterationLog . Doc $ DocIOAction msg) $> def
-
-
-
--- arbitraryIOInterpreter :: Members '[Error AppError, Logger, IO] effs => Sem (ArbitraryIO ': effs) a -> Sem effs a
--- arbitraryIOInterpreter =
---   let
---     handleException msg action = do
---                                 r <- send (E.try action)
---                                 case r of
---                                   Left (e :: IOException) -> throwError (AppIOError' ("Exception raised when executing arbituary IO action with message: " <> msg) e)
---                                   Right f -> pure f
---   in
---     interpret $ \(ArbitraryIO msg _ actn) -> logItem (IterationLog . Run $ IOAction msg) *> handleException msg actn
+arbitraryIOInterpreter :: forall effs a. Members '[Error AppError, Logger, Embed IO] effs => Sem (ArbitraryIO ': effs) a -> Sem effs a
+arbitraryIOInterpreter =
+  let
+    handleException :: forall b. Text -> IO b -> Sem effs b
+    handleException msg action = do
+                                r <- embed (E.try action)
+                                case r of
+                                  Left (e :: IOException) -> PE.throw (AppIOError' ("Exception raised when executing arbituary IO action with message: " <> msg) e)
+                                  Right f -> pure f
+  in
+    interpret $ \(ArbitraryIO msg _ actn) -> logItem (IterationLog . Run $ IOAction msg) *> handleException msg actn
 

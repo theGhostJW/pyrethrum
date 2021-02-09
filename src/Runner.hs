dd16e08cf01f9b8fe059f2a532b49776d15acd44
theGhostJW <theghostjw@gmail.com>
2021-02-10 06:05:01 +1100

    interim check in

--- src/Runner.hs
+++ src/Runner.hs
@@ -173,6 +173,14 @@ data RunParams e rc tc effs = RunParams {
   rc :: rc
 }
 
+data EndPointParams e rc tc effs = EndPointParams {
+  plan :: forall mo mi a. TestPlanBase e tc rc mo mi a effs,
+  filters :: FilterList rc tc,
+  itemIds :: Either ItemFilterError (S.Set Int) ,   
+  itemRunner :: forall as ds i. (ItemClass i ds, Show as, Show ds, ToJSON as, ToJSON ds) => ItemRunner e as ds i tc rc effs,
+  rc :: rc
+}
+
 -- TODO - Error handling especially outside tests 
 exeElm :: RunElement [] (Sem effs) () effs -> Sem effs ()
 exeElm elm = \case 
@@ -313,4 +321,4 @@ mkEndpointSem runParams@RunParams { filters } tstAddress iIds =
   in
     eitherf iIds
       (logItem . logRun . LP.Error . FilterError)
-      (\idSet -> mkSem (Just idSet) runParams { filters = allFilters })
+      (\idSet -> mkSem (Just idSet) (runParams { filters = allFilters } ::RunParams e rc tc effs ))

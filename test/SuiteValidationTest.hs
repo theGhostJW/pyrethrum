586ebb0102c5d4ec63b43460c92c34f3dfd45f63
theGhostJW <theghostjw@gmail.com>
2021-03-21 12:38:17 +1100

    delete cradel

--- test/SuiteValidationTest.hs
+++ test/SuiteValidationTest.hs
@@ -4,8 +4,11 @@ import MockSuite
 import DSL.Interpreter
 import Pyrelude
 import           Pyrelude.Test
+import DSL.LogProtocol
+import Common
 
 
+suiteResult :: Either (FrameworkError MyText) ([LogProtocolBase MyText], ())
 suiteResult = minInterpret happyRun
 
 unit_dummy = chk True

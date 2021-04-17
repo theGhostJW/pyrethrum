8f3cd63b98afad4e652900c37d97de2b328440cb
theGhostJW <theghostjw@gmail.com>
2021-04-17 14:30:49 +1000

    delete everything

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for simple-hpack
-
-## Unreleased changes
---
+++ src/Demo.hs
@@ -0,0 +1,8 @@
+module Demo where
+
+import Prelude
+
+Demo :: IO ()
+Demo = putStr "Hello World"
+-- Demo = putStr "Hello Hello"
+

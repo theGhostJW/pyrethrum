bca7c4e0388ee7e87c9ee59562fa225c041287d5
theGhostJW <theghostjw@gmail.com>
2021-10-10 08:41:51 +1100

    WIP C- before remove address from Tests element

--- src/RunnerBase.hs
+++ src/RunnerBase.hs
@@ -80,27 +80,18 @@ queryElm title' address =
 
     aeUndefined :: ho -> Sem effs ()
     aeUndefined ho = undefined
-    {- 
-      --     nextAddress :: Text -> RC.AddressElemType -> Address
-  --     nextAddress ttl et = push ttl et address
 
-  --     elmQuery :: forall hi' ho' c1. AddressElemType -> Text -> [Address -> hi' -> SuiteItem c1 hi' ho' effs [a]] -> [AddressedElm a]
-  --     elmQuery et ttl elms = elms >>= queryElm getItemTitle (nextAddress ttl et) . badCall
+    tstAddress :: a -> Address
+    tstAddress a = push (title' a) RC.Test address
 
-  --     hkQuery :: forall hi' ho' c1. Text -> [Address -> hi' -> SuiteItem c1 hi' ho' effs [a]] -> [AddressedElm a]
-  --     hkQuery = elmQuery RC.Hook
-  --  in \case
-
-  --       Root {rootElms} -> rootElms >>= queryElm getItemTitle address
-  -}
-  
-    nxtAddress :: a -> AddressElemType -> Address
-    nxtAddress a et = push (title' a) et address
+    -- elmQuery :: a -> AddressElemType -> SuiteItem c hi effs a -> [AddressedElm a]
+    -- elmQuery = queryElm title' address
   in
   \case
     Root {rootElms} -> rootElms >>= queryElm title' address
-    Tests {tests} -> (\a -> AddressedElm (nxtAddress a RC.Test) a) <$> tests address hiUndefined beUndefined aeUndefined 
-    Group {title = t, gElms = e} -> uu --elmQuery RC.Group t e
+    Tests {tests} -> (\a -> AddressedElm (tstAddress a) a) <$> tests address hiUndefined beUndefined aeUndefined 
+    -- SuiteItem c' hi effs a -> [AddressedElm  a]
+    Group {title = t, gElms = e} -> uu -- e >>= _
     BeforeAll {title = t, bhElms = e} -> uu --hkQuery t e
     BeforeEach {title' = t, bhElms' = e} -> uu --hkQuery t e
     AfterAll {title = t, ahElms = e} -> uu --hkQuery t e
@@ -140,119 +131,22 @@ querySuite' rc getTitle extractor suite =
 querySuite :: forall hi effs a. (a -> Text) -> SuiteItem Root' hi effs a -> [AddressedElm a]
 querySuite getItemTitle = uu --queryElm getItemTitle rootAddress
 
-  
-  -- uu --queryElm getItemTitle rootAddress
--- querySuite getItemTitle = queryElm getItemTitle rootAddress
-
--- queryTest ::
---   (Show i, ToJSON i, Show as, ToJSON as, Show ds, ToJSON ds, RC.ItemClass i ds) =>
---   Address ->
---   hi ->
---   (hi -> Sem effs ho) -> -- beforeEach
---   (ho -> Sem effs ()) -> -- AfterEach
---   Test e tc rc hi i as ds effs ->
---   a
--- queryTest = uu
-
-
--- querySuite :: (RC.Config rc, RC.Config tc) => (forall ho hi i as ds. (Show i, ToJSON i, Show as, ToJSON as, Show ds, ToJSON ds, HasField "checks" i (C.Checks ds), HasField "id" i Int, HasField "title" i Text) => rc -> tc -> i -> a) -> TestSuite e tc rc effs a
--- querySuite = uu
--- forall ho hi i as ds. (Show i, ToJSON i, Show as, ToJSON as, Show ds, ToJSON ds, HasField "checks" i (Check.Checks ds), HasField "id" i Int, HasField "title" i Text) => Address -> hi -> (hi -> Sem effs ho) -> (ho -> Sem effs ()) -> Test e tc rc ho i as ds effs -> a
-
-{-
-TODO
- SuiteItem Update
-  DONE - Add Hook
-  DONE - Add TestSuite Tests
-    * Thread Hook Output to Subelements (GADT)
-      * get compiling
-      * utilise input - ie change test runner
-    * Update Tests
-    * Explicit Hook connstructors
-    * Update Tests
-    * Query static data - items / checks / Config / Known Defects on Checks / Effects esp Hook effects how to query
-  Update Tests
-  Concurrency
-  Update Tests
-  Known Defect on interact / parse
-  Exception error handling
-  Update Tests
-  sample for actions that return values in the context of documentation
-  Update log interpretor
-  Runner that extracts test items - eg to report known errors
-  Update Tests
-  Update Demo
--}
-
--- concatTests :: SuiteItem hi ho effs t -> [t]
--- concatTests = uu
-
--- let
---   concat' ts = mconcat $ concatTests <$> ts
--- in
---   \case
---     (Tests f) -> [f]
---     (BeforeHook _ _ _ ts) -> concat' ts
---     (AfterHook _ _ _ ts) -> concat' ts
---     (Group _ ts) -> concat' ts
-
--- groupName :: SuiteItem hi ho effs a -> Maybe Text
--- groupName = \case
---   Tests _ -> Nothing
---   BeforeAll {} -> Nothing
---   BeforeEach {} -> Nothing
---   AfterAll {} -> Nothing
---   AfterEach {} -> Nothing
---   Root {} -> Nothing
---   Group t _ -> Just t
-
--- groupAddresses' :: [Text] -> Text -> SuiteItem hi ho effs a -> [Text]
--- groupAddresses' accum root el = uu
-
--- let
---   delim = "."
-
---   appendDelim :: Text -> Text -> Text
---   appendDelim p s = p <> (null p || null s ? empty $ delim) <> s
-
---   childAddresses :: [SuiteItem o effs a] -> [Text]
---   childAddresses  se = mconcat $ groupAddresses' accum root <$> se
--- in
---   case el of
---     Tests _ -> accum
-
---     BeforeHook _ _ _ subElems -> childAddresses subElems
-
---     AfterHook _ _ _ subElems -> childAddresses subElems
-
---     Group t subElems ->
---       let
---         address = appendDelim root t
---       in
---         address : mconcat (groupAddresses' accum address <$> subElems)
-
--- groupAddresses :: SuiteItem hi ho effs a -> [Text]
--- groupAddresses = groupAddresses' [] ""
-
--- data up to here want items to have title and validations
--- and config
-
 data CheckInfo = CheckInfo {
   header :: Text,
   expectation :: C.ResultExpectation
-}
+} deriving Show
 
 data ItemInfo = ItemInfo {
   id :: Int,
   title :: Text,
   checks :: [CheckInfo]
-}
+} deriving Show
 
 data TestInfo tc = TestInfo {
   title :: Text,
   config :: tc,
   itemInfo :: [ItemInfo]
-}
+} deriving Show
 
 info :: forall e tc rc hi i as ds effs. (RC.ItemClass i ds, RC.Config tc) => rc -> Test e tc rc hi i as ds effs -> TestInfo tc 
 info rc t = 

8b11834d42b72d011b5b564c678a060174139a57
theGhostJW <theghostjw@gmail.com>
2022-01-09 22:03:03 +1100

    WIP

--- src/Internal/ExeNodeLazy.hs
+++ src/Internal/ExeNodeLazy.hs
@@ -2,13 +2,13 @@
 
 module Internal.ExeNodeLazy where
 
+import Data.Function
 import Polysemy
-import Pyrelude (Text, fromMaybe, throw, uu, void, when, (?))
+import Pyrelude (Text, fromMaybe, isJust, maybef, throw, traverse_, unlessJust, uu, void, when, (?))
 import UnliftIO
 import UnliftIO.Concurrent
 import UnliftIO.STM
 import Prelude
-import Data.Function
 
 data CompletionStatus = Normal | Fault | Murdered deriving (Eq, Show)
 
@@ -38,19 +38,18 @@ data Node i o where
       rootChildren :: [Node () o]
     } ->
     Node () ()
-  Branch ::
+  Hook ::
     { branchParent :: Node i0 i,
       branchStatus :: IO (TVar BranchStatus),
       hook :: i -> IO o,
-      hookCache :: IO (TMVar o),
-      branchChildren :: [Node o o2],
+      hookResult :: IO (TMVar o),
+      hookChildren :: [Node o o2],
       branchRelease :: Int -> o -> IO ()
     } ->
     Node i o
   Fixture ::
     MonadUnliftIO m =>
-    { 
-      logStart :: IO (),
+    { logStart :: IO (),
       fixParent :: Node i0 i,
       iterations :: [i -> IO ()],
       logEnd :: IO ()
@@ -58,10 +57,16 @@ data Node i o where
     Node i ()
 
 data LoadedFixture = LoadedFixture
-  { 
+  { logStart :: IO (),
     fixStatus :: IO (TVar FixtureStatus),
     iterations :: IO (TVar [IO ()]),
-    activeThreads :: IO [IO ThreadId]
+    activeThreads :: IO [IO ThreadId],
+    logEnd :: IO ()
+  }
+
+data IndexedFixture = IndexedFixture
+  { index :: Int,
+    fixture :: LoadedFixture
   }
 
 {- TODO
@@ -74,8 +79,8 @@ data LoadedFixture = LoadedFixture
 
 -}
 
-wantLaunch :: TVar BranchStatus -> STM Bool
-wantLaunch status = do
+setReadyForLaunch :: TVar BranchStatus -> STM Bool
+setReadyForLaunch status = do
   s <- readTVar status
   s == Unintitalised
     ? do
@@ -83,29 +88,30 @@ wantLaunch status = do
       pure True
     $ pure False
 
--- launch only to be run when want launce has set
--- staus to initalising should only ever run once
+-- launch only to be run when want launch has set
+-- staus to initalising should only ever run once per branch
 -- TODO - test exception on output of branch parent and in resource aquisition
 launch :: Node i o -> IO ()
 launch =
   \case
     Root {} -> error "Not Implemented - Change types later"
     Fixture {} -> error "Not Implemented - Change types later"
-    Branch
+    Hook
       { branchParent,
         branchStatus,
         hook,
-        hookCache,
-        branchChildren,
+        hookResult,
+        hookChildren,
         branchRelease
       } -> do
         input <- outputWithLaunch branchParent
         bracket
           (hook input)
           (branchRelease 1 {- TODO implemnt pass through timeout for release -})
-          --  we need to loop here to stop the branch releasing before the fixture has run
+          -- we need to loop here to stop the branch releasing before the
+          -- fixture has run to completion
           \hookOut -> do
-            hkVal <- hookCache
+            hkVal <- hookResult
             status <- branchStatus
             atomically $ do
               putTMVar hkVal hookOut
@@ -121,41 +127,105 @@ outputWithLaunch :: Node i o -> IO o
 outputWithLaunch = \case
   Root {} -> pure ()
   Fixture {} -> pure ()
-  branch@Branch
+  branch@Hook
     { branchParent,
       branchStatus,
       hook,
-      hookCache,
-      branchChildren,
+      hookResult,
+      hookChildren,
       branchRelease
     } -> do
       bs <- branchStatus
-      wLaunch <- atomically $ wantLaunch bs
+      wantLaunch <- atomically $ setReadyForLaunch bs
       when
-        wLaunch
+        wantLaunch
         (void $ forkIO $ launch branch)
-      hc <- hookCache
-      atomically $ readTMVar hc
+      hc' <- hookResult
+      atomically $ readTMVar hc'
 
--- runFixture :: Node i o -> IO (TVar Status) -> IO (TMVar [o -> IO ()]) -> IO [m ThreadId] -> IO ()
--- runFixture parent status iterations threads = do
---   input <- outputWithLaunch parent
-
-loadFixture :: Node i o -> [o -> IO ()] -> IO LoadedFixture
-loadFixture parent iterations = do
+loadFixture :: Node i o -> [o -> IO ()] -> IO () -> IO () -> IO LoadedFixture
+loadFixture parent iterations logStart logEnd = do
   input <- outputWithLaunch parent
-  pure $ LoadedFixture { 
-    fixStatus = newTVarIO Pending,
-    iterations = newTVarIO ((input &) <$> iterations),
-    activeThreads = pure []
-  }
+  pure $
+    LoadedFixture
+      { logStart = logStart,
+        fixStatus = newTVarIO Pending,
+        iterations = newTVarIO ((input &) <$> iterations),
+        activeThreads = pure [],
+        logEnd = logEnd
+      }
+
 --
 
-fixtures :: Node i o -> [IO LoadedFixture]
-fixtures = \case
-  Root {rootChildren} -> rootChildren >>= fixtures
-  Branch {branchChildren} -> branchChildren >>= fixtures
-  Fixture logStart parent iterations logEnd -> [loadFixture parent iterations]
+mkFixtures :: Node i o -> [IO LoadedFixture]
+mkFixtures = \case
+  Root {rootChildren} -> rootChildren >>= mkFixtures
+  Hook {hookChildren} -> hookChildren >>= mkFixtures
+  Fixture logStart parent iterations logEnd -> [loadFixture parent iterations logStart logEnd]
 
--- mkRunFixture :: [Sem effs ()] -> RunFixture effs
--- mkRunFixture i = RunFixture i True
+data Executor = Executor
+  { maxThreads :: Int,
+    threadsInUse :: TVar Int,
+    fixtures :: TQueue IndexedFixture
+  }
+
+data Fork
+  = Fork LoadedFixture
+  | Pend
+  | RunComplete
+
+fixtureActive :: LoadedFixture -> IO Bool
+fixtureActive fx = do
+  fsv <- fixStatus fx
+  fs <- readTVarIO fsv
+  pure $ fs == Active
+
+firstActive :: [IO LoadedFixture] -> IO (Maybe LoadedFixture)
+firstActive = \case
+  [] -> pure Nothing
+  x : xs -> do
+    lf <- x
+    active <- fixtureActive lf
+    active
+      ? pure (Just lf)
+      $ firstActive xs
+
+pruneQueReturnNextReady :: TQueue LoadedFixture -> IO (Maybe LoadedFixture)
+pruneQueReturnNextReady q = do
+  let nxt = readTQueue q
+  uu
+
+-- nextFork' :: Executor -> IO (Maybe LoadedFixture)
+-- nextFork' ep = do
+--   atomically $ do
+--     let fxs = fixtures ep
+--     used <- readTVar $ threadsInUse ep
+--     let wantFork = used < maxThreads ep
+--     if wantFork
+--       then do
+--         fx <- readTQueue fxs
+--         let status = fixStatus fx
+--         pure Nothing
+--       else pure Nothing
+
+qFixture :: TQueue IndexedFixture -> (Int, LoadedFixture) -> STM ()
+qFixture q (idx, fixture) = writeTQueue q $ IndexedFixture idx fixture
+
+execute' :: Executor -> IO ()
+execute'
+  exe@Executor
+    { maxThreads,
+      threadsInUse,
+      fixtures = fixQ
+    } = uu
+
+execute :: Int -> Node i o -> IO ()
+execute maxThreads root = do
+  fxs <- sequence $ mkFixtures root
+  let idxFxs = zip [0 ..] fxs
+  -- create queue
+  q <- newTQueueIO
+  -- load queue
+  atomically $ traverse_ (qFixture q) idxFxs
+  initialThreadsInUse <- newTVarIO 0
+  execute' $ Executor maxThreads initialThreadsInUse q

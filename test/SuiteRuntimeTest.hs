1aad0286285c5f85f3f1e3b089cc2919f89dca76
theGhostJW <theghostjw@gmail.com>
2023-02-23 06:50:17 +1100

    WIP

--- test/SuiteRuntimeTest.hs
+++ test/SuiteRuntimeTest.hs
@@ -841,7 +841,6 @@ chkThreadLeafEventsStartEnd :: [[ExeEvent]] -> IO ()
 chkThreadLeafEventsStartEnd evts =
   traverse_ (`chkLeafEventsStartEnd` evts) threadedEventTypes
 
-
 chkFixtureChildren :: [[ExeEvent]] -> IO ()
 chkFixtureChildren =
   traverse_ chkEvents
@@ -1067,7 +1066,6 @@ data ChkErrAccum = ChkErrAccum
     matchedFails :: ST.Set Loc
   }
 
-
 chkHkReleased :: [ExeEvent] -> ExeEventType -> ExeEventType -> IO ()
 chkHkReleased evs hkType relType =
   {-
@@ -1130,83 +1128,97 @@ chkErrorPropagation evts =
     reconcileParents :: SThreadId -> IO ()
     reconcileParents tid =
       let cpMap' = cpMap tid
-          failMap = fails tid
-          etMap = evtTypeMap tid
+          failMap = fails $ Just tid
+          failsAllThreads = fails Nothing
           -- get the parent or if it is a grouping event
-          -- get it's paraent Grouping events (Groups and Fixtures) are effectively
-          -- ignored in the error propagation
+          -- get it's paraent Grouping events (Groups and Fixtures)
+          --  are effectively ignored in the error propagation as they
+          -- themsleves neither pass or fail
           truParent :: Loc -> Loc
           truParent parentLoc =
             let nxtParent = truParent $ lookupThrow "parent not found in child parent map" cpMap' parentLoc
-                parentIsGrouping = isGrouping $ lookupThrow "loc not found in event map" etMap parentLoc
+                parentIsGrouping = isGrouping $ lookupThrow "loc not found in event map" evtTypeMap parentLoc
              in (parentLoc == Root)
                   ? Root
                   $ (parentIsGrouping ? nxtParent $ parentLoc)
-          trueParentFailure pLoc = failMap M.!? truParent pLoc
        in traverse_
             ( \(chldLoc, pLoc) ->
-                isGrouping (lookupThrow "loc not found in event map" etMap chldLoc)
-                  ? pure ()
-                  $ failMap M.!? chldLoc
-                    & maybe
-                      ( -- the child event passed so parent must have passed
-                        trueParentFailure pLoc
-                          & maybe
-                            (pure ())
-                            ( error $
-                                "Child event passed when parent failed - error should have propagated\nchild\n"
-                                  <> ppShow chldLoc
-                                  <> "\nparent\n"
-                                  <> ppShow (truParent pLoc)
-                            )
-                      )
-                      ( -- the child event failed
-                        \case
-                          Fail {floc = childLoc, exception = childExcption} ->
-                            trueParentFailure pLoc
+                let trueParentFailure = failMap M.!? truParent pLoc
+                 in isGrouping (lookupThrow "loc not found in event map" evtTypeMap chldLoc)
+                      ? pure ()
+                      $ failMap M.!? chldLoc
+                        & maybe
+                          ( -- the child event passed so parent must have passed
+                            trueParentFailure
                               & maybe
                                 (pure ())
-                                (error $ "Child event failed (not propagated parent failure) when parent failed - parent error should have propagated\n" <> ppShow childLoc)
-                          ParentFail {floc = childloc, ploc, exception = childException} ->
-                            trueParentFailure pLoc
-                              & maybe
-                                (error $ "Child event has propagated parent failure when parent has not failed\n" <> ppShow childloc)
-                                ( \p ->
-                                    let pexcpt = SuiteRuntimeTest.exception p
-                                     in chkEq'
-                                          ( toS $
-                                              "Propagated excption does not equal parent exception for loc:\n"
-                                                <> ppShow childloc
-                                                <> "\nchild exception\n"
-                                                <> ppShow childException
-                                                <> "\nparent exception\n"
-                                                <> ppShow pexcpt
-                                          )
-                                          childException
-                                          pexcpt
+                                ( error $
+                                    "Child event passed when parent failed - error should have propagated\nchild\n"
+                                      <> ppShow chldLoc
+                                      <> "\nparent\n"
+                                      <> ppShow (truParent pLoc)
                                 )
-                      )
+                          )
+                          ( -- the child event failed
+                            \case
+                              Fail {floc = childLoc, exception = childExcption} ->
+                                trueParentFailure
+                                  & maybe
+                                    (pure ())
+                                    (error $ "Child event failed (not propagated parent failure) when parent failed - parent error should have propagated\n" <> ppShow childLoc)
+                              ParentFail {floc = childloc, ploc, exception = childException} ->
+                                let prntFail = trueParentFailure
+                                 in prntFail
+                                      & maybe
+                                        ( 
+                                        {- 
+                                          allow for special condition where onceHook has been failed due to failure in a 
+                                          concurrent thread which means onceHook will have propagated failure but parent
+                                          in this thread wont
+                                        -}
+                                          if isOnceEvent $ lookupThrow "eventType not found" evtTypeMap childloc then
+                                            error "Not implemented"
+                                          else
+                                          error $ "Child event has propagated parent failure when parent has not failed\n" <> ppShow childloc
+                                        )
+                                        ( \p ->
+                                            let pexcpt = SuiteRuntimeTest.exception p
+                                             in chkEq'
+                                                  ( toS $
+                                                      "Propagated excption does not equal parent exception for loc:\n"
+                                                        <> ppShow childloc
+                                                        <> "\nchild exception\n"
+                                                        <> ppShow childException
+                                                        <> "\nparent exception\n"
+                                                        <> ppShow pexcpt
+                                                  )
+                                                  childException
+                                                  pexcpt
+                                        )
+                          )
             )
             (M.toList cpMap')
 
     thrdIds = threadIds evts
-    evtTypeMap thrdId =
+    evtTypeMap =
       foldl'
         ( \acc ->
             \case
-              Start eet loc _n sti -> eventBelongsToThread thrdId sti eet ? M.insert loc eet acc $ acc
+              Start eet loc _idx sti -> M.insert loc eet acc
               _ -> acc
         )
         M.empty
         evts
     cpMap = actualChildParentMap evts
 
-    fails :: SThreadId -> M.Map Loc Fail
+    fails :: Maybe SThreadId -> M.Map Loc Fail
     fails targThread =
       foldl'
         ( \acc ->
             let rslt thistid' eventType newAcc =
-                  eventBelongsToThread targThread thistid' eventType ? newAcc $ acc
+                  targThread & maybe
+                    newAcc
+                    \targThread' -> eventBelongsToThread targThread' thistid' eventType ? newAcc $ acc
              in \case
                   StartExecution {} -> acc
                   Start {} -> acc
@@ -1439,10 +1451,12 @@ superSimplSuite =
     }
 
 -- $> unit_simple_singlelaw
+
 unit_simple_single :: IO ()
 unit_simple_single = runTest 1 superSimplSuite
 
 -- $> unit_simple_single_failure
+
 unit_simple_single_failure :: IO ()
 unit_simple_single_failure =
   runTest 1 $

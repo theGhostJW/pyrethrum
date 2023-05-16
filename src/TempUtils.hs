93106c3fee83fce5f6b318e1293624e2bfac829f
theGhostJW <theghostjw@gmail.com>
2023-05-17 07:24:32 +1000

    utc conversion

--- src/TempUtils.hs
+++ src/TempUtils.hs
@@ -1,7 +1,31 @@
 module TempUtils where
 
+import Chronos
+import Data.Time (Day (..), UTCTime (..))
 import Debug.Trace
+import PyrethrumExtras (uu)
 
--- functions that will be moved to pyrelude
-debugLines :: Show a => [a] -> IO ()
+-- TODO :: functions that will be moved to pyrelude - check if needed vs prettyprint functions
+debugLines :: (Show a) => [a] -> IO ()
 debugLines = traverse_ (traceIO . show)
+
+-- TODO :: UnTested Test
+utcToOffsetDateTime :: UTCTime -> OffsetDatetime
+utcToOffsetDateTime UTCTime{utctDay, utctDayTime} =
+  OffsetDatetime
+    { offsetDatetimeDatetime =
+        Datetime
+          { datetimeDate = toEnum $ fromEnum utctDay
+          , datetimeTime =
+              TimeOfDay
+                { timeOfDayHour = mins `div` 60
+                , timeOfDayMinute = mins `mod` 60
+                , timeOfDayNanoseconds = fromIntegral $ round $ (secs - fromIntegral iSecs) * 1000000000
+                }
+          }
+    , offsetDatetimeOffset = Offset 0
+    }
+ where
+  secs = realToFrac utctDayTime
+  iSecs = floor secs
+  mins = iSecs `div` 60

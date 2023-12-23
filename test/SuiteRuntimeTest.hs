1d67a74ad17a40aca6b3edd63c0565d8859fd6ad
theGhostJW <theghostjw@gmail.com>
2023-12-24 08:28:55 +1100

    remove log from git

--- test/SuiteRuntimeTest.hs
+++ test/SuiteRuntimeTest.hs
@@ -37,7 +37,7 @@ chkProperties _mxThrds ts evts = do
   putStrLn " checks done"
 
 
--- TODO:: reexport putStrLn et. al withtext conversion
+-- TODO:: reexport putStrLn et. al with text conversion
 chkAllOnceElementsStartedEnd :: Text ->  (LogItem -> Bool) ->  [LogItem] -> IO ()
 chkAllOnceElementsStartedEnd errSfx p l = do 
   --  putStrLn $ ppShowList trgEvnts

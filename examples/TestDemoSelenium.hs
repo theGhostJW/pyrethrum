956cf9c8df2c3017c0e2331e5e0d42b5c45a4920
theGhostJW <theghostjw@gmail.com>
2024-08-03 21:54:55 +0000

    start test demo

---
+++ examples/TestDemoSelenium.hs
@@ -0,0 +1,18 @@
+module TestDemoSelenium where
+
+{-
+ demo the following:
+  - single test suite with minimal selenium interpreter 
+    - when web ui interaction is implemented it will probably use a custom / different library due to licence
+  - read a value from "the internet"
+  - navigate between pages
+  - read a second value
+  - validator on value
+  - expect issue with laziness (if not why not)
+      - solve
+  - user steps
+  - run with documenter
+  - introduce action that uses value read from the internet
+    - should blow up documenter
+    - fix with doc* functions
+-}

358bb65448c84dd2c4ef4d8d5633566270def7f9
theGhostJW <theghostjw@gmail.com>
2024-08-11 21:31:36 +0000

    WIP

--- InstallFirfoxAndDriver.sh
+++ InstallFirfoxAndDriver.sh
@@ -4,6 +4,7 @@
 set -e
 
 # Variables
+# FIREFOX_URL="https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 GECKODRIVER_VERSION="v0.35.0"
 GECKODRIVER_URL="https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
 
@@ -20,13 +21,14 @@ sudo mv /tmp/geckodriver /usr/local/bin/
 sudo chown vscode:vscode /usr/local/bin/geckodriver
 sudo chmod +x /usr/local/bin/geckodriver
 
+#  genrated by chat gpt should not need because caninstall directly
 # Download and install Firefox
-wget $FIREFOX_URL -O /tmp/firefox.tar.bz2
-tar -xjf /tmp/firefox.tar.bz2 -C /tmp
-sudo mv /tmp/firefox /opt/firefox
-sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
-sudo chown -R vscode:vscode /opt/firefox
-sudo chmod -R +x /opt/firefox
+# wget $FIREFOX_URL -O /tmp/firefox.tar.bz2
+# tar -xjf /tmp/firefox.tar.bz2 -C /tmp
+# sudo mv /tmp/firefox /opt/firefox
+# sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+# sudo chown -R vscode:vscode /opt/firefox
+# sudo chmod -R +x /opt/firefox
 
 # Clean up
 rm /tmp/geckodriver.tar.gz

c1826d6e2cd4ad0e6a2ffa1dd341623ba79b9a37
theGhostJW <theghostjw@gmail.com>
2024-09-26 21:43:09 +0000

    WIP

--- src/Internal/Logging.hs
+++ src/Internal/Logging.hs
@@ -41,7 +41,7 @@ runWithLogger :: forall l lx. LogControls l lx -> (LoggerSource l -> IO ()) -> I
 runWithLogger
   LogControls
     { sink,
-      aggregator,
+      expander,
       logWorker,
       stopWorker
     }
@@ -59,20 +59,21 @@ runWithLogger
         )
     where
       mkNewLogger :: IO (l -> IO ())
-      mkNewLogger = mkLogger aggregator sink <$> UnliftIO.newIORef (-1) <*> P.myThreadId
+      mkNewLogger = mkLogger expander sink <$> UnliftIO.newIORef (-1) <*> P.myThreadId
 
 -- adds log index and thread id to loggable event and sends it to the sink
 mkLogger :: forall l lxp. (C.ThreadId -> Int -> l -> lxp) -> (lxp -> IO ()) -> IORef Int -> ThreadId -> l -> IO ()
-mkLogger aggregator sink idxRef thrdId logEvnt = do
+mkLogger expander sink idxRef thrdId logEvnt = do
+  -- TODO: Add timestamp - need to change type of expander
   tc <- readIORef idxRef
   let nxt = succ tc
-  finally (sink $ aggregator (C.mkThreadId thrdId) nxt logEvnt) $ writeIORef idxRef nxt
+  finally (sink $ expander (C.mkThreadId thrdId) nxt logEvnt) $ writeIORef idxRef nxt
 
 -- TODO:: Logger should be wrapped in an except that sets non-zero exit code on failure
 
-data LogControls l lx = LogControls
-  { aggregator :: C.ThreadId -> Int -> l -> lx,
-    sink :: lx -> IO (),
+data LogControls log expandedLog = LogControls
+  { expander :: C.ThreadId -> Int -> log -> expandedLog,
+    sink :: expandedLog -> IO (),
     logWorker :: IO (),
     stopWorker :: IO ()
   }
@@ -86,7 +87,7 @@ q2List qu = reverse <$> recurse [] qu
         >>= maybe (pure l) (\e -> recurse (e : l) q)
 
 testLogControls' :: forall l lx. (Show lx) => (C.ThreadId -> Int -> l -> lx) -> Bool -> IO (LogControls l lx, STM [lx])
-testLogControls' aggregator wantConsole = do
+testLogControls' expander wantConsole = do
   chn <- newTChanIO
   log <- newTQueueIO
 
@@ -107,7 +108,7 @@ testLogControls' aggregator wantConsole = do
           writeTChan chn $ Just eventLog
           writeTQueue log eventLog
 
-  pure (LogControls {..}, q2List log)
+  pure (LogControls {logWorker, stopWorker, sink, expander}, q2List log)
 
 {- Logging functions specialised to Event type -}
 

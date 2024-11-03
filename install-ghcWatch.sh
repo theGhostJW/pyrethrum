609fd346cb6b335490971011a9c712b1bae856d8
theGhostJW <theghostjw@gmail.com>
2024-11-03 16:01:46 +1100

    fix ghci watch script

--- install-ghcWatch.sh
+++ install-ghcWatch.sh
@@ -6,7 +6,7 @@
 set -e
 
 # Variables
-GHC_WATCH_URL="wget https://github.com/MercuryTechnologies/ghciwatch/releases/download/v1.0.1/ghciwatch-aarch64-linux -O /usr/local/bin/ghciwatch"
+GHC_WATCH_URL="wget https://github.com/MercuryTechnologies/ghciwatch/releases/download/v1.0.1/ghciwatch-x86_64-linux -O /usr/local/bin/ghciwatch"
 
 wget $GHC_WATCH_URL -O /usr/local/bin/ghciwatch
 sudo chmod +x /usr/local/bin/ghciwatch

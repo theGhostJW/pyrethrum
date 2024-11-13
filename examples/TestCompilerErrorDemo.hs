cd7cdf6fbc59e6408ea3eae46258232e50f13404
theGhostJW <theghostjw@gmail.com>
2024-11-13 19:56:09 +1100

    Add Demo

---
+++ examples/TestCompilerErrorDemo.hs
@@ -0,0 +1,99 @@
+module TestCompilerErrorDemo where
+
+import Check
+import Core (ParseException)
+import DSL.Internal.NodeLog (NodeLog, Path (NodePath))
+import DSL.OutEffect (Out)
+import Effectful as EF
+  ( Eff,
+    type (:>),
+  )
+import PyrethrumBase
+import PyrethrumExtras (txt)
+import WebDriverEffect as WE
+import WebDriverSpec (DriverStatus (Ready), Selector (CSS))
+import Filter (Filters(..))
+import Internal.SuiteRuntime (ThreadCount(..))
+import Internal.Logging qualified as L
+import WebDriverPure (seconds)
+import DSL.Logging (log)
+
+
+suite :: Suite
+suite =
+  [Fixture (NodePath "WebDriverDemo" "test") test]
+
+runDemo :: SuiteRunner -> Suite -> IO ()
+runDemo runner suite' = do 
+  (logControls, _logLst) <- L.testLogActions True
+  runner suite' Unfiltered defaultRunConfig (ThreadCount 1) logControls
+
+-- start geckodriver first: geckodriver &
+runIODemo :: Suite -> IO ()
+runIODemo = runDemo ioRunner
+
+-- ############### Test Case ###################
+
+-- >>> runIODemo suite
+
+test :: Fixture ()
+test = Full config action parse items
+
+config :: FixtureConfig
+config = FxCfg "test" DeepRegression
+
+driver_status :: (WebUI :> es, Out NodeLog :> es) => Eff es DriverStatus
+driver_status = do 
+  status <- driverStatus
+  log $ "the driver status is: " <> txt status
+  pure status
+
+action :: (WebUI :> es, Out NodeLog :> es) => RunConfig -> Data -> Eff es AS
+action _rc i = do
+  log i.title
+  status <- driver_status
+  log $ "the driver status is (from test): " <> txt status
+  ses <- newSession
+  maximiseWindow ses
+  go ses _theInternet
+  link <- findElem ses _checkBoxesLinkCss
+  checkButtonText <- readElem ses link
+  clickElem ses link
+  -- so we can see the navigation worked
+  sleep $ 5 * seconds
+  killSession ses
+  pure $ AS {status, checkButtonText}
+ 
+data AS = AS
+  { status :: DriverStatus,
+    checkButtonText :: Text
+  }
+  deriving (Show)
+
+data DS = DS
+  { status :: DriverStatus,
+    checkButtonText :: Text
+  }
+  deriving (Show)
+
+data Data = Item
+  { id :: Int,
+    title :: Text,
+    checks :: Checks DS
+  }
+  deriving (Show, Read)
+
+parse :: AS -> Either ParseException DS
+parse AS {..} = pure $ DS {..}
+
+items :: RunConfig -> DataSource Data
+items _rc =
+  ItemList
+    [ Item
+        { id = 1,
+          title = "test the internet",
+          checks =
+            chk "Driver is ready" ((== Ready) . (.status))
+              <> chk "Checkboxes text as expected" ((== "Checkboxes") . (.checkButtonText))
+        }
+    ]
